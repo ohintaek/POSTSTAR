f67b6de67020c81c19033650f42f92c6fc3d5e09
OH INTAEK <flytaek@gmail.com>
2017-12-21 15:25:03 +0900

    PostStar.Common 프로젝트 추가

---
+++ PostStar.Common/Util/PerformanceInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostStar.Common.Util
+{
+    [DllImport("psapi.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PerformanceInformation
+    {
+        public int Size;
+        public IntPtr CommitTotal;
+        public IntPtr CommitLimit;
+        public IntPtr CommitPeak;
+        public IntPtr PhysicalTotal;
+        public IntPtr PhysicalAvailable;
+        public IntPtr SystemCache;
+        public IntPtr KernelTotal;
+        public IntPtr KernelPaged;
+        public IntPtr KernelNonPaged;
+        public IntPtr PageSize;
+        public int HandlesCount;
+        public int ProcessCount;
+        public int ThreadCount;
+    }
+
+    /// <summary>
+    /// 메모리 사용률을 구한다.
+    /// </summary>
+    /// <returns></returns>
+    public static Int64 GetMemoryUseRate()
+    {
+        PerformanceInformation pi = new PerformanceInformation();
+        if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)) == false)
+            return 0;
+
+        //// 현재 사용가능 메모리양
+        Int64 available = pi.PhysicalAvailable.ToInt64();
+
+        //// 전체메모리양
+        Int64 total = pi.PhysicalTotal.ToInt64();
+
+        //// 현재 사용률
+        return (total - available) * 100 / total;
+    }
+}
+
+}
---
+++ PostStar.Common/Util/SystemInfo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostStar.Common.Util
+{
+    /// <summary>
+    /// 시스템 정보
+    /// </summary>
+    public class SystemInfo
+    {
+        static PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+        /// <summary>
+        /// CPU 사용량을 구한다.
+        /// </summary>
+        /// <returns></returns>
+        public static int GetCpuUseRate()
+        {
+            return (int)cpuCounter.NextValue();
+        }
+
+        /// <summary>
+        /// Disk 사용률을 구한다.
+        /// </summary>
+        /// <returns></returns>
+        public static int GetDiskUseRate(String driveName)
+        {
+            int useRate = 0;
+
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    if (drive.IsReady == false)
+                        continue;
+
+                    if (drive.Name.ToUpper().Equals(driveName) == false)
+                        continue;
+
+                    useRate = Convert.ToInt32((drive.TotalSize - drive.TotalFreeSpace) * 100 / drive.TotalSize);
+                    break;
+                }
+            }
+            catch
+            {
+                useRate = 0;
+            }
+
+            return useRate;
+        }
+
+        /// <summary>
+        /// 실행중인 프로세스 목록을 구한다.
+        /// </summary>
+        /// <returns></returns>
+        public static Process[] GetProcessList()
+        {
+            return Process.GetProcesses();
+        }
+    }
+
+}
